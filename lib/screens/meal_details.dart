2b725829f9dcb0002a386f479d149cd7799e346c
Avishka Umesh <umesh.avishka@gmail.com>
2023-07-15 16:19:54 +0530

    added animation for favorite button

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -22,7 +22,19 @@ class MealDetailsScreen extends ConsumerWidget {
           title: Text(meal.title),
           actions: [
             IconButton(
-              icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+              icon: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (child, animation) {
+                  return RotationTransition(
+                    turns: Tween(begin: 0.5, end: 1.0).animate(animation),
+                    child: child,
+                  );
+                },
+                child: Icon(
+                  isFavorite ? Icons.star : Icons.star_border,
+                  key: ValueKey(isFavorite),
+                ),
+              ),
               onPressed: () {
                 final wasAdded = ref
                     .read(favoritesMealsProvider.notifier)
