3170df61a21b4ebbfbfbc5e0beb9497ce94472b4
Avishka Umesh <umesh.avishka@gmail.com>
2023-07-15 12:46:47 +0530

    added dynamic icon to favorites

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -14,12 +14,15 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoritesMealsProvider);
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
         appBar: AppBar(
           title: Text(meal.title),
           actions: [
             IconButton(
-              icon: const Icon(Icons.star),
+              icon: Icon(isFavorite ? Icons.star : Icons.star_border),
               onPressed: () {
                 final wasAdded = ref
                     .read(favoritesMealsProvider.notifier)
